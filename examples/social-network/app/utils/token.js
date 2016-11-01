fbd61b4b115cf55cb77c8a19d42884cce0df4ab2
Nick Schot <nickschot@gmail.com>
2016-11-01 10:43:46 +0100

    Update jsonwebtoken package Fix function definition for token endpoint Don't export expiresIn

---
+++ examples/social-network/app/utils/token.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+//NOTE: you should put this in an environment variable instead of the codebase
+export const secret = 'shhhhhhared-secret';
+const expiresIn = 60 * 60;
+
+export function getNewToken(data){
+    return new Promise((resolve, reject) => {
+        jwt.sign(data, secret, { expiresIn: expiresIn }, (err, token) => {
+            if(err) {
+                return reject(err);
+            }
+
+            resolve(token);
+        });
+    });
+}
